6bc270383a7a76312e33693d663972f4f66738bf
Lorenzo Sicilia <lorenzo.sicilia@gmail.com>
2019-01-02 22:37:50 +0000

    Add NullSchema

---
+++ src/NullSchema.test.js
@@ -0,0 +1,34 @@
+const { NullSchema } = require('./NullSchema')
+const { FluentSchema } = require('./FluentSchema')
+
+describe('NullSchema', () => {
+  it('defined', () => {
+    expect(NullSchema).toBeDefined()
+  })
+
+  describe('constructor', () => {
+    it('without params', () => {
+      expect(NullSchema().valueOf()).toEqual({
+        type: 'null',
+      })
+    })
+    it('from FluentSchema', () => {
+      expect(
+        FluentSchema()
+          .asNull()
+          .valueOf()
+      ).toEqual({
+        $schema: 'http://json-schema.org/draft-07/schema#',
+        type: 'null',
+      })
+    })
+  })
+
+  it('sets a null type to the prop', () => {
+    expect(
+      FluentSchema()
+        .prop('prop', FluentSchema().asNull())
+        .valueOf().properties.prop.type
+    ).toEqual('null')
+  })
+})
